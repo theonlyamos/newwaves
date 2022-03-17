e55e789ea5f8d4fab7de242160870becb26c3d71
Amos Amissah <theonlyamos@gmail.com>
2022-03-17 18:19:28 +0000

    redesigned minimal header + nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,18 +7,19 @@ import Img1 from '../../assets/images/photo-1642425146609-6c8ff4589d18.jpg';
 import Img2 from '../../assets/images/photo-1644329770639-1a20809b82a3.jpg';
 import Img3 from '../../assets/images/photo-1643101811638-c44424d967fb.jpg';
 import Img4 from '../../assets/images/photo-1643639779508-4c60952c058c.jpg';
+import { HiArrowRight } from 'react-icons/hi';
 
 export const Header = ({})=>{
 
     return (
-        <header className='relative flex justify-center gradient-bg-white'>
-            <NavBar className={'absolute container px-5 sm:px-10 md:px-20 pt-7 flex justify-between items-center z-50'}>
+        <header className='relative flex justify-center gradient-bg-welcome'>
+            <NavBar className={'absolute w-full px-5 sm:px-10 md:px-20 flex justify-between items-center z-50 bg-white opacity-80'}>
                 <Brand logo={Logo} url='/' className={'rotate-90 z-10 filter-grayscale'}/>
                 <Nav className={'z-50 text-white'}>
-                    <NavItem url='/' title='Home' active={true} className="mt-5 text-black"/>
-                    <NavItem url='/' title='About' className="mt-5 text-black"/>
-                    <NavItem url='/' title='Services' className="mt-5 text-black"/>
-                    <NavItem url='/' title='Contact Us' className="mt-5 text-black"/>
+                    <NavItem url='/' title='Home' active={true} className="mt-5 py-4 md:py-0 w-full md:w-fit hover:bg-gray-200 md:text-xs text-center uppercase text-black letter-spacing-1"/>
+                    <NavItem url='/' title='Services' className="mt-5 py-4 md:py-0 w-full md:w-fit hover:bg-gray-200 md:text-xs text-center uppercase text-black letter-spacing-1"/>
+                    <NavItem url='/' title='Portfolio' className="mt-5 py-4 md:py-0 w-full md:w-fit hover:bg-gray-200 md:text-xs text-center uppercase text-black letter-spacing-1"/>
+                    <NavItem url='/' title='Contact Us' className="mt-5 py-4 md:py-0 w-full md:w-fit hover:bg-gray-200 md:text-xs text-center uppercase text-black letter-spacing-1"/>
                 </Nav>
             </NavBar>
             {/*
@@ -29,11 +30,18 @@ export const Header = ({})=>{
                 <CarouselItem img={Img4} title="Mobile Applications Development" color="black" animation='slide-left' key="3"/>
             </Carousel>
             */}
-            <div className={`flex flex-col justify-center  min-h-screen w-full`}>
-                <h1 className={`text-5xl text-center font-extrabold text-gradient-red fade-in`}>
+            <div className={`w-full md:w-8/12 lg:w-7/12 px-5 text-center md:text-left flex flex-col justify-center min-h-screen`}>
+                {/*<img src={Img4} className="absolute top-0 left-0 min-h-screen"/>*/}
+                <h1 className={`text-5xl fade-in z-50`} style={{lineHeight: '4rem'}}>
                     New Waves Technologies
                 </h1>
-                <h3 className={`w-full text-3xl text-center italic text-gradient-yellow fade-in`}>Discovering limits</h3>
+                <div className='mt-2 z-50 uppercase'>
+                    <h3 className={`text-md fade-in letter-spacing-1`}>Discovering limits, Eroding boundaries</h3>
+                </div>
+                <a href="/" className="flex justify-center items-center mx-auto md:mx-0 px-7 py-3 animate-pulse hover:animate-none hover:shadow-lg text-md bg-blue-900 hover:bg-blue-800 mt-2 text-gray-300 hover:text-white uppercase w-fit">
+                    Learn More
+                    <HiArrowRight className='ml-3'/>
+                </a>
             </div>
         </header>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, createContext, useContext, useEffect } from 'react';
+
 import { HiMenu } from 'react-icons/hi';
 import { FaTimes } from 'react-icons/fa';
 
+const NavContext = createContext({
+    menuOpen: false,
+    setMenuOpen: ()=>{}
+});
+
 export const Brand = ({logo, title, url, className}) => {
   return (
-    <a href={url} title={title} className={` ${className}`} style={{filter: 'grayscale(1)'}}>
+    <a href={url} title={title} className={` ${className}`}>
       {logo && (<img src={logo} alt={title}/>)}
       {title && (
         <span>{title}</span>
@@ -24,10 +30,9 @@ export const NavItem = ({
       <a
         href={url}
         title={title}
-        className={`mx-2 ${active
+        className={`mx-4 ${active
         ? 'font-bold'
         : ''}`}
-        style={{'letterSpacing': '1px'}}
         >
         {title}
       </a>
@@ -36,41 +41,42 @@ export const NavItem = ({
 }
 
 export const Nav = ({className, children}) => {
-    const [menuOpen, setMenuOpen] = useState(false)
-    const screenWidth = document.documentElement.clientWidth
+    const {menuOpen, setMenuOpen} = useContext(NavContext)
+
     return (
-        <>
-            <ul className={`list-none p-0 hidden md:flex`}>
-                {children}
-            </ul>
-            {menuOpen && (
-                <ul className={`list-none p-0 z-50 animate-slide-in flex flex-col justify-center items-center absolute bg-white text-black top-0 -right-10 min-h-screen w-full md:w-1/2'`}>
-                    <FaTimes 
-                        className='cursor-pointer text-black absolute top-5 left-5' 
-                        fontSize={'40px'} 
-                        onClick={()=>{setMenuOpen(false)}}
-                    />
-                    {children}
-                    
-                </ul>
-            )}
-            {!menuOpen && (
-                <HiMenu 
-                    className='cursor-pointer text-black hover:text-gray-800 md:hidden' 
-                    fontSize={'40px'} 
-                    onClick={()=>{setMenuOpen(true)}}
-                />
-            )}
+      <>
+          <ul className={`list-none p-0 hidden md:flex`}>
+              {children}
+          </ul>
+          {menuOpen && (
+              <ul className={`list-none p-0 z-50 animate-slide-in flex flex-col justify-center items-center absolute bg-white text-black top-0 left-0 min-h-screen w-full md:w-1/2'`}>
+                  <FaTimes 
+                      className='cursor-pointer text-gray-700 hover:text-black absolute top-5 left-5' 
+                      fontSize={'40px'} 
+                      onClick={()=>{setMenuOpen(false)}}
+                  />
+                  {children}
+              </ul>
+          )}
+          {!menuOpen && (
+              <HiMenu 
+                  className='cursor-pointer text-black hover:text-gray-800 md:hidden' 
+                  fontSize={'40px'} 
+                  onClick={()=>{setMenuOpen(true)}}
+              />
+          )}
         </>
     )
 }
 
 export const NavBar = ({className, children}) => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  
   return (
-    <>
-        <nav className={`justify-between z-50 text-white ${className}`}>
-            {children}
-        </nav>
-    </>
+    <NavContext.Provider value={{menuOpen, setMenuOpen}}>
+      <nav className={`justify-between z-50 text-white ${className} ${menuOpen ? 'min-h-screen' : ''}`}>
+          {children}
+      </nav>
+    </NavContext.Provider>
   )
 }
