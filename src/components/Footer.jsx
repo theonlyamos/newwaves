7fe4c9543a34bdc4d9a8d3ccf1f8be38e0b6a30b
Amos Amissah <theonlyamos@gmail.com>
2022-03-17 19:22:51 +0000

    started footer section. Logo, social media icons and copyright done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
-import * as React from 'react';
+import { Brand } from './Nav';
 
+import Logo from '../../assets/icons/android/android-launchericon-96-96.png';
+import { FaFacebookSquare, FaGooglePlusSquare, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa';
 
 export const Footer = ({})=>{
+    const year = (new Date()).getFullYear()
     return (
-        <footer className='footer py-20 w-full flex flex-col justify-center gradient-bg-footer' id='footer'>
-            <div>
-                <h1 className='text-center font-bold text-5xl text-gradient-white'>
-                    Footer
-                </h1>
+        <footer className='footer py-10 w-full gradient-bg-footer' id='footer'>
+            <div className='w-full px-5 sm:px-10 md:px-20 flex justify-between items-center'>
+                <Brand logo={Logo} url='/' className={'rotate-90 z-10 filter-grayscale'}/>
+                <div className="flex space-x-4">
+                    <a href="/">
+                        <FaFacebookSquare className='text-white' fontSize={'2.5rem'}/>
+                    </a>
+                    <a href="">
+                        <FaInstagramSquare className='text-white' fontSize={'2.5rem'}/>
+                    </a>
+                    <a href="">
+                        <FaGooglePlusSquare className='text-white' fontSize={'2.5rem'}/>
+                    </a>
+                    <a href="">
+                        <FaTwitterSquare className='text-white' fontSize={'2.5rem'}/>
+                    </a>
+                </div>
+            </div>
+            <div className="w-full mt-5 text-center text-white letter-spacing-1 text-xs">
+                <span>Copyright&copy;{year} New Waves Technologies. All Rights Reserved.</span>
             </div>
         </footer>
     )
