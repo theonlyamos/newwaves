1e042185b56d9d73a06221d864ee61ce758ee245
Amos Amissah <theonlyamos@gmail.com>
2022-03-18 02:02:20 +0000

    added some fonts, fixed line-height from tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { FaFacebookSquare, FaGooglePlusSquare, FaInstagramSquare, FaTwitterSquar
 export const Footer = ({})=>{
     const year = (new Date()).getFullYear()
     return (
-        <footer className='footer py-10 w-full gradient-bg-footer' id='footer'>
+        <footer className='footer pt-10 w-full gradient-bg-footer' id='footer'>
             <div className='w-full px-5 sm:px-10 md:px-20 flex justify-between items-center'>
                 <Brand logo={Logo} url='/' className={'rotate-90 z-10 filter-grayscale'}/>
                 <div className="flex space-x-4">
@@ -24,7 +24,7 @@ export const Footer = ({})=>{
                     </a>
                 </div>
             </div>
-            <div className="w-full mt-5 text-center text-white letter-spacing-1 text-xs">
+            <div className="w-full py-5 mt-5 text-center text-white letter-spacing-1 text-xs border-t-2 border-slate-600 bg-black">
                 <span>Copyright&copy;{year} New Waves Technologies. All Rights Reserved.</span>
             </div>
         </footer>
